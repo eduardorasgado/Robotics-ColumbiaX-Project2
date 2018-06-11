3114210735ab93baa5b0f9f2fa0a33e7bd202416
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-11 11:44:44 -0500

    kinetics_node.py debugged

--- scripts/kinematics_node.py
+++ scripts/kinematics_node.py
@@ -3,6 +3,7 @@
 import rospy
 import numpy
 import tf
+import tf2_ros
 import geometry_msgs.msg
 
 def message_from_transform(T):
@@ -11,7 +12,7 @@ def message_from_transform(T):
 	translation = tf.transformations.translation_from_matrix(T)
 	msg.translation.x = translation[0]
 	msg.translation.y = translation[1]
-	msg.trnaslation.z = translation[2]
+	msg.translation.z = translation[2]
 	msg.rotation.x = q[0]
 	msg.rotation.y = q[1]
 	msg.rotation.z = q[2]
@@ -21,31 +22,44 @@ def message_from_transform(T):
 
 def publish_transforms():
 	#----T1------------------------
+	#one big transform matrix -> rotation matrix and translation one
 	T1 = tf.transformations.concatenate_matrices(
-			tf.transformations.translation_matrix((1.0, 1.0, 0.0))
-			tf.transformations.quaternion_matrix(
-				tf.transformations.quaternion_from_euler(1.0, 1.0, 1.0)
-				)
-		)
+		#translation tx, ty, tz
+		tf.transformations.translation_matrix((1.0, 1.0, 0.0)),
+		#transform quaternion into a rotation matrix
+		tf.transformations.quaternion_matrix(
+			#we have our rotations expresed in euler angles
+			#we just are converting into a quaternion
+			tf.transformations.quaternion_from_euler(1.0, 1.0, 1.0)))
+
+	#This just tells ROS who is this transform between
 	T1_stamped = geometry_msgs.msg.TransformStamped()
 	T1_stamped.header.stamp = rospy.Time.now()
+	#this is transform from the frame world(base)
 	T1_stamped.header.frame_id = "world"
+	#To frame F1
 	T1_stamped.child_frame_id = "F1"
 	T1_stamped.transform = message_from_transform(T1)
 	br.sendTransform(T1_stamped)
+	
 	#----T2-------------------------
+	#second transform same strcuture as above
 	T2 = tf.transformations.concatenate_matrices(
-		tf.transformations.traslation_matrix((1.0, 0.0 0.0))
+		#the translation matrix tx,ty,tz
+		tf.transformations.translation_matrix((1.0, 0.0, 0.0)),
+		#quaternion to rotation matrix
 		tf.transformations.quaternion_matrix(
-			tf.transformations.quaternion_about_axis(1.57, (1, 0, 0))
-			)
-		)
+			#in quaternion
+			#rotation by 90 deg(it is in radians) around the x axis
+			tf.transformations.quaternion_about_axis(1.57, (1, 0, 0))))
 	T2_stamped = geometry_msgs.msg.TransformStamped()
 	T2_stamped.header.stamp = rospy.Time.now()
+	#tranform from frame F1 into frame F2
 	T2_stamped.header.frame_id = "F1"
 	T2_stamped.child_frame_id = "F2"
 	T2_stamped.transform = message_from_transform(T2)
-	br.sendtransform(T2_stamped)
+	#using the broadcaster to publish
+	br.sendTransform(T2_stamped)
 
 if __name__ == '__main__':
 	rospy.init_node("tf2_examples")
